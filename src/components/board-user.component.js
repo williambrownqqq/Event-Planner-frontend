9e8a8661e77ab6f3ff4d13eaf093f64967cdfed6
lesha <iamalexzanchenko@gmail.com>
2024-05-07 18:16:33 +0300

    monitoring board

--- src/components/board-user.component.js
+++ src/components/board-user.component.js
@@ -25,21 +25,21 @@ function BoardUser() {
   }, []); // Empty array means this effect runs once on mount
 
 
-    // Function to map content to chart data
-    const mapContentToChartData = (content) => {
+    const mapContentTo1ChartData = (content) => {
       if (!content) return null;
   
-      const labels = content.map(item => item.date);
+      // const dates = content.map(item => item.date);
+      // const uniqueDates = [...new Set(dates)];
+      //  console.log(uniqueDates);
+
       const data = content.map(item => item.value);
-      const products = content.map(item => item.product);
       console.log(data);
-      console.log(labels);
-  
+
       return {
-        labels,
+        labels: ["01.2024","02.2024","03.2024","04.2024","05.2024"],
         datasets: [
           {
-            label: "Revenue",
+            label: "Electricity Generation, Gw",
             data,
             backgroundColor: 'rgba(75,192,192,0.4)', // Example color
             borderColor: 'rgba(75,192,192,1)', // Example color
@@ -49,69 +49,125 @@ function BoardUser() {
       };
     };
 
-    const chartData = mapContentToChartData(content);
+     const chartData = mapContentTo1ChartData(content);
+
+     const mapContentTo2ChartData = (content) => {
+      if (!content) return null;
+    
+      const products = content.map(item => item.product);
+    
+      const counts = {};
+      products.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+    
+      const keys = Object.keys(counts);
+      const values = Object.values(counts);
+      const slicekeys = keys.slice(1,7);
+      const slicevalues = values.slice(1,7);
+    
+      // Generate a color for each unique product
+      const backgroundColors = keys.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.4)`);
+      //const borderColors = keys.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`);
+    
+      return {
+        labels: slicekeys,
+        datasets: [
+          {
+            label: "Types of electricity generation facilities",
+            data: slicevalues,
+            backgroundColor: backgroundColors,
+            //borderColor: borderColors,
+            borderWidth: 1,
+          },
+        ],
+      };
+    };
+      const chart2Data = mapContentTo2ChartData(content);
+      
+    
+      const mapContentTo3ChartData = (content) => {
+        if (!content) return null;
+        
+        const data = content.map(item => item.value);
+        const revdata = data.map(item => item * 1.5);
+        console.log(data);
+
+        const products = content.map(item => item.product);
+        // const uniqueProducts = [...new Set(products)];
+      
+        const counts = {};
+        products.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+      
+        const keys = Object.keys(counts);
+        const values = Object.values(counts);
+        const slicekeys = keys.slice(1,7);
+        const slicevalues = values.slice(1,7);
+      
+        // Generate a color for each unique product
+        const backgroundColors = keys.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.4)`);
+        //const borderColors = keys.map(() => `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 1)`);
+      
+        return {
+          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov","Dec"],
+          datasets: [
+            {
+              label: "Electricity export power, Kw",
+              data,
+              backgroundColor: "#FC4100",
+              //borderColor: borderColors,
+              borderWidth: 1,
+            },
+            // {
+            //   label: "Electricity export power, Kw",
+            //   revdata,
+            //   backgroundColor: "#064FF0",
+            //   borderWidth: 1,
+            // },
+          ],
+        };
+      };
+        const chart3Data = mapContentTo3ChartData(content);  
 
 return (  
-  <section id="pattern" class="pattern">
-      <ul class="grid">
-        <li>
-          <div>
-            {chartData && <Bar data={chartData} />}
-          </div>
-        </li>
-        <li>
-        {/* <div>
-            <Doughnut 
+  <div className="chart-container-box" >
+    
+     {/* <div>
+             <Bar
               data={{
-                labels: ["A", "B", "C"],
+                labels: ["01.2024","02.2024","03.2024","04.2024","05.2024"],
                 datasets: [
                   {
-                    label: "Revenue",
-                    data: [200,300,400],
+                    label: "Electricity Generation, Gw",
+                    data: [1,2,3,4,5], // content.map(item => item.value)
                     },
                   ],
                 }}/>
-          </div> */}
-           <div>
-            {chartData && <Doughnut data={chartData} />}
-          </div>
-        </li>
-        <li>
-          <div>
-          {/* <div>
-            <Line 
-              data={{
-                labels: ["A", "B", "C"],
-                datasets: [
-                  {
-                    label: "Revenue",
-                    data: [200,300,400],
-                    },
-                  ],
-                }}/>
-          </div> */}
-          <div>
-            {chartData && <Line data={chartData} />}
-          </div>
-          </div>
-        </li>
+     </div> */}
 
-        <li>
-        <div>
-            <Bubble
-              data={{
-                labels: ["A", "B", "C"],
-                datasets: [
-                  {
-                    label: "Revenue",
-                    data: [200,300,400],
-                    },
-                  ],
-                }}/>
-          </div>
-        </li>
-      </ul>
-    </section>
+      <div className="chart-container">
+      <h3>Electricity generation power, Gw</h3>
+        {chartData && <Bar data={chartData} />}
+     
+      </div>
+
+{/*   
+      <div className="chart-container">
+        {chart3Data && <Bubble data={chart3Data} />}
+        <h3>Electricity export power, Kw</h3>
+      </div> */}
+
+      <div className="chart-container">
+      <h3>Electricity export power, Kw</h3>
+        {chart3Data && <Line data={chart3Data} />}
+     
+      </div>
+
+      <div className="chart-container">
+      <h3>Types of electricity generation facilities</h3>
+        {chart2Data && <Doughnut data={chart2Data} />}
+       
+      </div>
+  </div>
+       
   );
 }
 export default BoardUser;
